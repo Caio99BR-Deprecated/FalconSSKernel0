63cc9c76cd05b357463584c507093049270992c2
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-10-30 12:58:58 -0200

    patch.sh: Update to latest version
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- patch.sh
+++ patch.sh
@@ -2,61 +2,112 @@
 # Writen by Caio Oliveira aka Caio99BR <caiooliveirafarias0@gmail.com>
 
 echo "  | Live Git Patcher"
-echo "  |"
-echo "  | Put the link after '. patch.sh'"
-echo "  | To use this script"
+
+# Check option of user and transform to script
+for _u2t in "${@}"
+do
+	if [[ "${_u2t}" == "-h" || "${_u2t}" == "--help" ]]
+	then
+		echo "  |"
+		echo "  | Put the link after '. patch.sh'"
+		echo "  | To use this script"
+		echo "  |"
+		echo "  | Options:"
+		echo "  | -h    | --help  | To show this message"
+		echo "  | -caf  | --caf   | Bypass addition of '.patch' in links"
+		echo "  | -3    | --3     | Use 'git am -3' to apply patchs"
+	fi
+	if [[ "${_u2t}" == "-caf" || "${_u2t}" == "--caf" ]]
+	then
+		_option_caf="enable"
+	fi
+	if [[ "${_u2t}" == "-3" || "${_u2t}" == "--3" ]]
+	then
+		_option_am3=" -3"
+	fi
+done
+
 echo "  |"
 echo "  | Number of patch's: ${#}"
+
 c="0"
 for link in ${@}
 do
-	c=$[$c+1]
-	echo "  |"
-	echo "  | Patch #${c}"
-	if [ "${link}" == *".patch" ]
+	if [[ "${link}" == "http"* ]]
 	then
-		nl="${link}"
-	else
-		nl="${link}.patch"
-	fi
-	path_patch="/tmp/patch.sh-${c}.patch"
-	echo "  |"
-	echo "  | Downloading"
-	curl -# -o ${path_patch} ${nl}
-	if [ -f "${path_patch}" ]
-	then
+		c=$[$c+1]
 		echo "  |"
-		echo "  | Patching"
-		git am -3 ${path_patch}
-		if [ "${?}" == "0" ]
+		echo "  | Patch #${c}"
+		if [[ "${link}" == *".patch" || "${_option_caf}" == "enable" ]]
+		then
+			nl="${link}"
+		else
+			nl="${link}.patch"
+		fi
+		patch_filename="patch.sh-${c}.patch"
+		path_patch="/tmp"
+		echo "  |"
+		echo "  | Downloading"
+		curl -# -o ${path_patch}/${patch_filename} ${nl}
+		if [ -f "${path_patch}/${patch_filename}" ]
 		then
 			echo "  |"
-			echo "  | Patch (${c}/${#})"
+			echo "  | Patching"
+			git am${_option_am3} ${path_patch}/${patch_filename}
+			if [ "${?}" == "0" ]
+			then
+				echo "  |"
+				echo "  | Patch (${c}/${#})"
+			else
+				echo "  |"
+				echo "  | Something not worked good in patch #${c}"
+				echo "  | Aborting 'git am' process"
+				if ! [ "${c}" == "${#}" ]
+				then
+					if [ ${#} -gt "1" ]; then
+						echo "  |"
+						echo "  | Passing to next patch"
+					fi
+				fi
+				echo $(tput sgr0)
+				git am --abort
+			fi
 		else
 			echo "  |"
-			echo "  | Something not worked good in patch #${c}"
-			echo "  | Aborting 'git am' process"
-			git am --abort
+			echo "  | Patch not downloaded, check internet or link"
 			if ! [ "${c}" == "${#}" ]
 			then
-				if [ ${#} -gt "1" ]
-				then
+				if [ ${#} -gt "1" ]; then
 					echo "  |"
 					echo "  | Passing to next patch"
 				fi
 			fi
 		fi
+		rm -rf ${path_patch}/${patch_filename}
 	else
-		echo "  |"
-		echo "  | Patch not downloaded, check internet or link"
-		if ! [ "${c}" == "${#}" ]
+		if [[ "${link}" == "--caf" || "${_option_caf}" == "-caf" ]]
 		then
-			if [ ${#} -gt "1" ]
+			# WORKAROUND
+			echo > /dev/null
+		elif [[ "${link}" == "--3" || "${_option_caf}" == "-3" ]]
+		then
+			# WORKAROUND
+			echo > /dev/null
+		else
+			echo "  |"
+			echo "  | This link:"
+			echo "  | ${link}"
+			echo "  | not is a 'HTTP' link"
+			echo "  | This script only accept 'HTTP' links for now"
+			if ! [ "${c}" == "${#}" ]
 			then
-				echo "  |"
-				echo "  | Passing to next patch"
+				if [ ${#} -gt "1" ]; then
+					echo "  |"
+					echo "  | Passing to next patch"
+				fi
 			fi
 		fi
 	fi
-	rm -rf ${path_patch}
 done
+
+unset _option_caf _option_am3
